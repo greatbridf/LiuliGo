f80b4c499ac87c157659fe278eef67180ae08fcc
greatbridf <guobingfang20040616@gmail.com>
2019-05-19 23:21:51 +0800

    feat(handler/content): make content service work

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 func main() {
 	//http.HandleFunc("/", handlers.HandleRoot)
 	http.HandleFunc("/articles", handlers.HandleArticles)
-	//http.HandleFunc("/content", handlers.HandleContent)
+	http.HandleFunc("/content", handlers.HandleContent)
 	//http.HandleFunc("/magnet", handlers.HandleMagnet)
 	//http.HandleFunc("/delete", handlers.HandleDelete)
 	err := http.ListenAndServe(":14250", nil)
