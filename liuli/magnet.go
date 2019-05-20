94ee94ea1aafab277a3c9846bc7b667e3ead1478
greatbridf <guobingfang20040616@gmail.com>
2019-05-20 14:32:26 +0800

    fix(manget): use new interface

--- liuli/magnet.go
+++ liuli/magnet.go
@@ -12,7 +12,7 @@ type Magnets []string
 // GetMagnet get magnet link from content
 // Returns array
 func GetMagnet(id string) (Magnets, error) {
-	link := "https://interface.greatbridf.top/liuli?req=content&id=" + id
+	link := "https://interface.greatbridf.top/liuli/content?id=" + id
 	doc, _ := goquery.NewDocument(link)
 	doc.Find("a").Remove()
 	doc.Find("img").Remove()
